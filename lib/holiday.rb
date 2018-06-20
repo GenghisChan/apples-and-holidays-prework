55908772685b05121ae9790ce527a7f433c3aee5
Christofer Chan <devchumbing@gmail.com>
2018-06-20 00:50:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,8 +52,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |season, items|
-    puts items
-
+    items
+end
 end
 
 def all_supplies_in_holidays(holiday_hash)
