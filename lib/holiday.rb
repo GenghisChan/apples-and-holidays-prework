9686bda878c6403c1587fb6b106e1b368be73d6a
Christofer Chan <devchumbing@gmail.com>
2018-06-20 00:15:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,6 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     if key == :summer
       holiday_hash[:summer][:fourth_of_july][1]
     end
+    end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
